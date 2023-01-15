deb90618305c3e4660a3112b73f4b4a3137ed947
Егор Бойко <ego.boiko2018@yandex.ru>
2023-01-16 00:40:29 +0300

    new

--- homework.py
+++ homework.py
@@ -63,8 +63,6 @@ class Running(Training):
     CALORIES_MEAN_SPEED_MULTIPLIER: int = 18
     CALORIES_MEAN_SPEED_SHIFT: float = 1.79
 
-    
-
     def get_spent_calories(self) -> float:
         return ((self.CALORIES_MEAN_SPEED_MULTIPLIER * self.get_mean_speed()
                 + self.CALORIES_MEAN_SPEED_SHIFT) * self.weight / self.M_IN_KM
